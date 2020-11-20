81d00c6b66178059a36ddfc667cc527f9ded8045
FediSalhi <fadi.salhi@outlook.fr>
2020-11-20 15:59:12 +0300

    first commit

---
+++ source/main.c
@@ -0,0 +1,32 @@
+/*
+ * main.c
+ *
+ *  Created on: 20 November 2020
+ *      Author: Fedi Salhi
+ */
+
+#include<msp430fr5994.h>
+
+void main() {
+
+    /* initializations */
+
+
+    /* infinite loop */
+
+    while(1) {
+
+
+
+
+
+    }
+
+
+
+
+
+}
+
+
+
