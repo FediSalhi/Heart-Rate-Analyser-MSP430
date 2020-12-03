22fc978b8d26a02e68035dee8f60e831ef649782
FediSalhi <fadi.salhi@outlook.fr>
2020-12-03 23:40:43 +0300

    simple_communication modified

--- header/simple_communication.h
+++ header/simple_communication.h
@@ -28,14 +28,32 @@
 
 /* simple communication packet structure */
 typedef struct {
-    UINT8_t synchronous_1;
-    UINT8_t synchronous_2;
+    UINT8_t synchronous_1_u8;
+    UINT8_t synchronous_2_u8;
     UINT8_t source_device_u8;
     UINT8_t target_device_u8;
     UINT8_t paket_type_t;
-    UINT32_t data_length;
+    UINT32_t data_length_u8;
     UINT8_t data[MAX_DATA_LENGTH];
 }Simple_communication_paket_t;
+
+typedef enum {
+    CAPTURING_SYNCHRONOUS_1,
+    CAPTURING_SYNCHRONOUS_2,
+    CAPTURING_SOURCE_DEVICE,
+    CAPTURING_TARGET_DEVICE,
+    CAPTURING_PACKET_TYPE,
+    CAPTURING_DATA_LENGTH,
+    CAPTURING_DATA
+}Packet_capturing_status_t;
+
+
+typedef struct {
+    Packet_capturing_status_t paket_capturing_status_t;
+    UINT16_t captured_packet_num_u16;
+    UINT16_t solved_packet_number_u16;
+    Simple_communication_paket_t simple_com_packet_t;
+}Serial_uart_channel_t;
 
 
 void Cont_heart_rate_measurement_packet_create(Simple_communication_paket_t* paket_t,
