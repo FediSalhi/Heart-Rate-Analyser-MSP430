23673af886ddc4a014eb65edbbc3c951827f7325
FediSalhi <fadi.salhi@outlook.fr>
2020-12-14 23:42:17 +0300

    adc

--- source/main.c
+++ source/main.c
@@ -12,6 +12,12 @@
 #include "driverlib.h"
 
 
+/************** Global Variables ********/
+uint16_t GL_data = 0;
+
+
+
+
 /*************** prototypes *************/
 
 void adc_init();
@@ -26,13 +32,20 @@ void main() {
     /* Hold Watchdog Timer */
     WDT_A_hold(WDT_A_BASE);
 
+    /* unlock pins */
+    PMM_unlockLPM5();
+
     /* Init ADC */
     adc_init(); //to be implemented
 
 
-
+    __bis_SR_register(GIE);
     while(1) {
         /* infinite loop */
+
+        ADC12_B_startConversion(ADC12_B_BASE,
+                                ADC12_B_START_AT_ADC12MEM0,
+                                ADC12_B_SINGLECHANNEL);
 
         measurement_loop(&heart_rate_measurement_t); //TODO: define this func."
 
@@ -56,8 +69,22 @@ void adc_init()
     param.clockSourcePredivider         = ADC12_B_CLOCKPREDIVIDER__1;
     param.internalChannelMap            = 0; // no mapping of internal signals
 
+
+    /* memory settings */
+    ADC12_B_configureMemoryParam mem_param      = {0};
+    mem_param.memoryBufferControlIndex          = ADC12_B_MEMORY_0;
+    mem_param.inputSourceSelect                 = ADC12_B_INPUT_A2;
+    mem_param.refVoltageSourceSelect            = ADC12_B_VREFPOS_AVCC_VREFNEG_VSS;
+    mem_param.endOfSequence                     = ADC12_B_NOTENDOFSEQUENCE;
+    mem_param.windowComparatorSelect            = ADC12_B_WINDOW_COMPARATOR_DISABLE;
+    mem_param.differentialModeSelect            = ADC12_B_DIFFERENTIAL_MODE_DISABLE;
+
     ADC12_B_init(ADC12_B_BASE,
                  &param);
+
+    /* use adc in low power mode */
+    ADC12_B_setAdcPowerMode(ADC12_B_BASE,
+                            ADC12_B_LOWPOWERMODE);
 
     /* enabling the ADC */
     ADC12_B_enable(ADC12_B_BASE);
@@ -69,6 +96,27 @@ void adc_init()
                                ADC12_B_MULTIPLESAMPLESDISABLE);
 
     /* configure memory buffer */
+    ADC12_B_configureMemory(ADC12_B_BASE,
+                            &mem_param);
+
+    /* clear interrupt flag */
+    ADC12_B_clearInterrupt(ADC12_B_BASE,
+                           2,
+                           ADC12_B_IFG0); //Mem0
+
+    /* enable memory buffer 0 interrupt */
+    ADC12_B_enableInterrupt(ADC12_B_BASE,
+                            ADC12_B_IE0,
+                            0,
+                            0);
+
+    /* start sampling and conversion */
+    ADC12_B_startConversion(ADC12_B_BASE,
+                            ADC12_B_START_AT_ADC12MEM0,
+                            ADC12_B_SINGLECHANNEL);
+
+    GL_data = ADC12_B_getResults(ADC12_B_BASE,
+                                  ADC12_B_MEMORY_0);
 
 }
 
@@ -92,6 +140,21 @@ void measurement_loop(Heart_rate_masurement_t* heart_rate_measurement_t) {
 
 }
 
+
+#pragma vector = ADC12_B_VECTOR
+__interrupt void myADC_ISR (void)
+{
+    switch(ADC12IV)
+    {
+    case 12:
+        GL_data = ADC12_B_getResults(ADC12_B_BASE,
+                                      ADC12_B_MEMORY_0);
+
+        __bic_SR_register_on_exit(LPM0_bits);
+
+        break;
+    }
+}
 
 
 
